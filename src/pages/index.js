0bc686a170ff453e619163334c632714a4738633
Shashank <snaik2064@gmail.com>
2019-10-30 15:05:08 +0530

    Navbar design complete.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-import { Link } from 'gatsby'
 import Layout from '../components/Layout'
 
 export default () => (
   <Layout>
-    Hello world!
-    <Link to="/blog/">blog</Link>
+    <div>Hello it's a gatsby app.</div>
   </Layout>
 )
