e25e83a46afe09e79b635b08016d37635c937997
Shashank <snaik2064@gmail.com>
2019-10-31 12:13:13 +0530

    About Us section complete.

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,13 +1,35 @@
 import React from 'react';
 
 import Title from '../Title';
+import styels from '../../css/about.module.css';
+import img from '../../images/defaultBcg.jpeg';
 
 const About = () => {
   return (
-    <div>
-      Hello from about
+    <section className={styels.about}>
       <Title title="about" subtitle="us" />
-    </div>
+      <div className={styels.aboutCenter}>
+        <article className={styels.aboutImg}>
+          <div className={styels.imgContainer}>
+            <img src={img} alt="about company" />
+          </div>
+        </article>
+        <article className={styels.aboutInfo}>
+          <h4>explore the difference</h4>
+        </article>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quo
+          laudantium doloremque?
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quo
+          laudantium doloremque?
+        </p>
+        <button type="button" className="btn-primary">
+          read more
+        </button>
+      </div>
+    </section>
   );
 };
 
