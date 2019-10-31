64be499ba37651de3993cca080402e17b5ce9ef4
Shashank <snaik2064@gmail.com>
2019-10-31 17:02:59 +0530

    siteMetadata added to gatsby config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,13 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: 'Backroads',
+    description:
+      'Expolre worldwide tours & discver what makes each of them unique.Forget your daily routine & say yes to adventure',
+    author: '@johndoe',
+    data: {
+      name: 'john',
+      age: 24,
+    },
+  },
   plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
 };
