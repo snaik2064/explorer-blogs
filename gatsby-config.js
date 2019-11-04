cce19383b6b94daca881befb3203ab41328218f7
Shashank <snaik2064@gmail.com>
2019-11-04 11:14:19 +0530

    Contentful integration complete.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: 'Backroads',
@@ -15,6 +19,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-plugin-sass`,
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,7 +6,11 @@ const Contact = () => {
     <section className={styles.contact}>
       <Title title="contact" subtitle="us" />
       <div className={styles.center}>
-        <form className={styles.form}>
+        <form
+          className={styles.form}
+          action="https://formspree.io/shashank.n@amazatic.com"
+          method="POST"
+        >
           <div>
             <label htmlFor="name">name</label>
             <input
