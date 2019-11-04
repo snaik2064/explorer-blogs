0f3e1987cf7d3bdc1512c03ceff1250f621493d3
Shashank <snaik2064@gmail.com>
2019-11-04 16:07:50 +0530

    Tour component design and integration complete.

--- src/components/Tours/Tour.js
+++ src/components/Tours/Tour.js
@@ -1,7 +1,39 @@
 import React from 'react';
 
-const Tour = () => {
-  return <div>Single tour</div>;
+import Image from 'gatsby-image';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import { FaMap } from 'react-icons/fa';
+
+import styles from '../../css/tour.module.css';
+
+const Tour = ({ tour }) => {
+  const { name, price, country, days, slug, images } = tour;
+
+  let mainImage = images[0].fluid;
+
+  return (
+    <article className={styles.tour}>
+      <div className={styles.imgContainer}>
+        <Image fluid={mainImage} className={styles.img} alt="single tour" />
+        <AniLink fade className={styles.link} to={`/tours/${slug}`}>
+          details
+        </AniLink>
+      </div>
+      <div className={styles.footer}>
+        <h3>{name}</h3>
+        <div className={styles.info}>
+          <h4 className={styles.country}>
+            <FaMap className={styles.icon} />
+            {country}
+          </h4>
+          <div className={styles.details}>
+            <h6>{days}</h6>
+            <h6>from ${price}</h6>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default Tour;
