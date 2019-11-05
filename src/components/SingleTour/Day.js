b319674f889adcaff5989b8f723adecd4883482d
Shashank <snaik2064@gmail.com>
2019-11-05 15:30:42 +0530

    Single day component design & integration complete.

--- src/components/SingleTour/Day.js
+++ src/components/SingleTour/Day.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Day = () => {
-  return <div>From Day</div>;
+import { FaAngleDown } from 'react-icons/fa';
+
+import styles from '../../css/day.module.css';
+
+const Day = ({ day, info }) => {
+  const [showInfo, setInfo] = useState(false);
+
+  const toggleInfo = () => {
+    setInfo(showInfo => !showInfo);
+  };
+
+  return (
+    <article className={styles.day}>
+      <h4>
+        {day}
+        <button className={styles.btn} onClick={toggleInfo}>
+          <FaAngleDown />
+        </button>
+      </h4>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Day;
