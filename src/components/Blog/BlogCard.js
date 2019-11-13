332bd21a0d3371bff056e08b5a1dcbb1de041a8d
Shashank <snaik2064@gmail.com>
2019-11-13 12:32:21 +0530

    Blog card UI integrated with contentful

--- src/components/Blog/BlogCard.js
+++ src/components/Blog/BlogCard.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const BlogCard = () => {
-  return <div>Blog card</div>;
+import styles from '../../css/blog-card.module.css';
+import Image from 'gatsby-image';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+
+const BlogCard = ({ blog }) => {
+  const { slug, title, image, published } = blog;
+
+  return (
+    <article className={styles.blog}>
+      <div className={styles.imgContainer}>
+        <Image fluid={image.fluid} className={styles.img} alt="Single post" />
+        <AniLink fade className={styles.link} to={`/blog/${slug}`}>
+          read more
+        </AniLink>
+        <h6 className={styles.date}>{published}</h6>
+      </div>
+      <div className={styles.footer}>
+        <h4>{title}</h4>
+      </div>
+    </article>
+  );
 };
 
 export default BlogCard;
