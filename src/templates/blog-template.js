742e1b83216c42cc807766568febeadb6e93a19f
Shashank <snaik2064@gmail.com>
2019-11-13 15:40:56 +0530

    Rich text probelm solved with rich-text-react-renderer.

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,8 +1,11 @@
 import React from 'react';
+
 import { graphql } from 'gatsby';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
 import Layout from '../components/Layout';
 import styles from '../css/single-blog.module.css';
-import AniLink from 'gatsby-plugin-transition-link/AniLink';
 const Blog = ({ data }) => {
   const {
     title,
@@ -10,12 +13,48 @@ const Blog = ({ data }) => {
     text: { json },
   } = data.post;
 
+  const options = {
+    renderNode: {
+      'embedded-asset-block': node => {
+        return (
+          <div className="rich">
+            <h3>Awesome</h3>
+            <img
+              width="400"
+              src={node.data.target.fields.file['en-US'].url}
+              alt=""
+            />
+            <p>images not provided</p>
+          </div>
+        );
+      },
+      'embedded-entry-block': node => {
+        const { title, image, text } = node.data.target.fields;
+        return (
+          <div>
+            <br />
+            <h1>Other post: {title['en-US']}</h1>
+            <img
+              width="400"
+              src={image['en-US'].fields.file['en-US'].url}
+              alt="other"
+            />
+            {documentToReactComponents(text['en-US'])}
+          </div>
+        );
+      },
+    },
+  };
+
   return (
     <Layout>
       <section className={styles.blog}>
         <div className={styles.center}>
           <h1>{title}</h1>
           <h4>published at : {published}</h4>
+          <article className={styles.post}>
+            {documentToReactComponents(json, options)}
+          </article>
 
           <AniLink fade to="/blog" className="btn-primary">
             all posts
